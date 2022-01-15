778d390049b34d0a5be734f0be8fb553831cf0aa
Buddy Ugwumba <56096174+bugwumba@users.noreply.github.com>
2022-01-15 00:23:20 -0800

    Delete outputVecFunc.m

--- outputVecFunc.m
+++
@@ -1,6 +0,0 @@
-% output vector function
-function y = outputVecFunc(x)
-     % Remember that you need a dot for element by element implentation
-    % since x is a vector
- y = sin(x)./(1 + exp(2.*x));
-end
