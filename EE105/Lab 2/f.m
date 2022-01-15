0a2e0164e1c9e3a7a8daf7f4ab0cc5e50789b99e
Buddy Ugwumba <56096174+bugwumba@users.noreply.github.com>
2022-01-15 00:39:43 -0800

    Delete EE105 directory

--- EE105/Lab 2/f.m
+++
@@ -1,7 +0,0 @@
-function dx = f(t,x,u)
-% x -- [2xn] column vector
-% u -- [1xn] vector
-A = [0 1; -16 -6];
-B = [0; 16];
-dx = A*x + B*u;
-end
--- EE105/Lab 2/f_ode23.m
+++
@@ -1,8 +0,0 @@
-function dx = f_ode23(t,x,omg)
-% x -- [2xn] column vector
-% u -- [1xn] vector
-A = [0,1; -16,-6];
-B = [0; 16];
-u = sin(omg*t);
-dx = A*x + B*u;
-end
